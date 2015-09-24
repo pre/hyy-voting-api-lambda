0fe40566817355c4dedb6aa440adc98af1c4dc47
Petrus Repo <petrus.repo@iki.fi>
2015-09-24 08:09:49 +0300

    feat (Auth) Issue a JWT for a valid sign in token

--- app/session-routes.js
+++ app/session-routes.js
@@ -5,50 +5,52 @@ var express = require('express'),
 
 var app = module.exports = express.Router();
 
-// XXX: This should be a database of users :).
-var users = [{
+// XXX: a database of issued tokens :).
+var issuedTokens = [{
   id: 1,
-  username: 'pre',
-  password: 'pre'
+  token: 'abc',
+  user: {
+    id: 100,
+    email: "lehti.koivu@helsinki.fi",
+    dontIncludeThis: "do not include this value"
+  },
+  response: {
+    goodToken: "from node",
+    faculty: {
+      name: "Humanistinen Nodekunta"
+    },
+    candidates: {
+      "url": "/mock_api/hum_tdk-candidates.json"
+    },
+    alliances: {
+      url: "/mock_api/hum_tdk-alliances.json"
+    }
+  }
 }];
 
 function createToken(user) {
-  return jwt.sign(_.omit(user, 'password'), config.secret, { expiresInMinutes: 60*5 });
+  return jwt.sign(_.omit(user, 'dontIncludeThis'), config.secret, { expiresInMinutes: 60*5 });
 }
 
-app.post('/users', function(req, res) {
-  if (!req.body.username || !req.body.password) {
-    return res.status(400).send("You must send the username and the password");
-  }
-  if (_.find(users, {username: req.body.username})) {
-   return res.status(400).send("A user with that username already exists");
-  }
-
-  var profile = _.pick(req.body, 'username', 'password', 'extra');
-  profile.id = _.max(users, 'id').id + 1;
-
-  users.push(profile);
-
-  res.status(201).send({
-    id_token: createToken(profile)
-  });
-});
+function error(message) {
+  return { error: { message: message } }
+}
 
 app.post('/sessions/create', function(req, res) {
-  if (!req.body.username || !req.body.password) {
-    return res.status(400).send("Username or password missing");
+  var token = req.body.token;
+
+  if (!token) {
+    return res.status(400).send(error("Token missing"));
   }
 
-  var user = _.find(users, {username: req.body.username});
-  if (!user) {
-    return res.status(401).send("The username or password don't match");
-  }
+  var issuedToken = _.find(issuedTokens, {token: token});
 
-  if (!user.password === req.body.password) {
-    return res.status(401).send("The username or password don't match");
+  if (!issuedToken) {
+    return res.status(401).send(error("Token is invalid"));
   }
 
   res.status(201).send({
-    id_token: createToken(user)
+    jwt: createToken(issuedToken.user),
+    details: issuedToken.response
   });
 });
