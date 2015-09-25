c0cab4a0aa04faf5a557b4e7a20310ccf9b5a06e
Petrus Repo <petrus.repo@iki.fi>
2015-09-25 08:27:16 +0300

    chore (Vote) Create protected endpoint for votes

--- app/protected-routes.js
+++ app/protected-routes.js
@@ -8,8 +8,14 @@ var jwtCheck = jwt({
   secret: config.secret
 });
 
-app.use('/api/protected', jwtCheck);
+app.use('/votes', jwtCheck);
 
-app.get('/api/protected/test', function(req, res) {
+app.get('/votes/test', function(req, res) {
   res.status(200).send("it works in private!");
+});
+
+// This endpoint is idempotent, ie. subsequent requests will not create
+// a new vote, instead the existing vote is updated if needed.
+app.post('/votes', function(req, res) {
+  res.status(200).send("successful vote!");
 });
