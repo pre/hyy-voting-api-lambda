eff2e8e458388ceb140c201bf384b46a131ec49f
Petrus Repo <petrus.repo@iki.fi>
2015-09-30 19:54:34 +0300

    chore (jwt) Mimic Lambda

---
+++ app/lambdas/vote.js
@@ -0,0 +1,10 @@
+var config  = require('../config'),
+    jwt     = require('jsonwebtoken');
+
+exports.handler = function(event, context) {
+  var decoded = jwt.verify(context.token, config.secret, {});
+
+  console.log('jwt decoded: ', decoded);
+
+  return context.done('(inside lambda) it works! user: ' + decoded.id);
+};
